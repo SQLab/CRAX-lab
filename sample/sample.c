e6d6d354feaf5c81d097afb10522cb64b1a64a55
ddaa <chengtc@cs.nctu.edu.tw>
2015-09-09 08:57:12 +0800

    add samples

---
+++ sample/sample.c
@@ -0,0 +1,14 @@
+#include <s2e.h>
+
+void foo(char *a)
+{
+	char b[40];
+	strcpy(b, a);
+}
+
+int main(int argc, char** argv)
+{
+	char *a = argv[1];
+	s2e_make_concolic(a, 300, "crax");
+	foo(a);
+}
